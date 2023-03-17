7115d46ab1b0d75582be815d0ca177d7f4785be9
Akshay Khandelwal <39028584+akshaykhandelwal0710@users.noreply.github.com>
2023-03-17 12:47:00 +0530

    Update segtree_with_lazy.cpp

--- segtree_with_lazy.cpp
+++ segtree_with_lazy.cpp
@@ -1,97 +1,142 @@
+struct Lazy{
+    // lazy data values
+
+    Lazy(){
+        // initialise
+
+    }
+};
+
+struct Data{
+    // data values
+
+    Data(){
+        // initialise
+        
+    }
+};
+
+Data idt = Data(); // additive identity
+Data join_res = Data(); // store result of joining here
+Data result = Data(); // any result from segtree gets stored here
+
+Lazy idt_l = Lazy(); // additive identity
+Lazy join_res_l = Lazy(); // store result of joining two lazy values here
+
+void join(Data &left, Data &right){
+    // function for joining
+
+}
+
+void join_lazy(Lazy &nw, Lazy &old){
+    // function for combining two lazy values
+    
+}
+
+void combine(Lazy &lazy, Data &val){
+    // change data as per lazy value
+
+}
+
+void assign(Data &from, Data &to){
+    // copy all the data in 'from' to 'to'
+
+}
+
 struct segmentTree{
     int low, high;
+    bool init;
     segmentTree *left;
     segmentTree *right;
-    $type$ val, lazy;
+    Data val;
+    Lazy lazy;
+    bool prop;
 
     void update_val(){
-        val = left->val + right->val;
-    }
-
-    segmentTree(int l, int h, $type$ default_val){
-        low = l;
-        high = h;
-        lazy = 0;
-        if (l < h){
-            int mid = (low + high)/2;
-            left = (segmentTree *)malloc(sizeof(segmentTree));
-            right = (segmentTree *)malloc(sizeof(segmentTree));
-            *left = segmentTree(low, mid, default_val);
-            *right = segmentTree(mid+1, high, default_val);
-            update_val();
-        }
-        else{
-            val = default_val;
-        }
-    }
-
-    segmentTree(int l, int h, vector<$type$> &arr){
-        low = l;
-        high = h;
-        lazy = 0;
-        if (l < h){
-            int mid = (low + high)/2;
-            left = (segmentTree *)malloc(sizeof(segmentTree));
-            right = (segmentTree *)malloc(sizeof(segmentTree));
-            *left = segmentTree(low, mid, arr);
-            *right = segmentTree(mid+1, high, arr);
-            update_val();
-        }
-        else{
-            val = arr[l];
-        }
+        join(left->val, right->val);
+        assign(join_res, val);
     }
 
     void push_down(){
-        val += lazy*(high-low+1);
-        if (low < high){
-            left->lazy += lazy;
-            right->lazy += lazy;
-        }
-        lazy = 0;
+        if (!prop) return;
+        combine(lazy, val);
+        prop = false;
+        if (low == high) return;
+        left->prop = right->prop = true;
+        join_lazy( lazy, left->lazy);
+        left->lazy = join_res_l;
+        join_lazy(lazy, right->lazy);
+        right->lazy = join_res_l;
     }
 
-    $type$ get(){
-        push_down();
-        return val;
-    }
-
-    $type$ get_range(int l, int h){
-        push_down();
-        if (low > h || high < l){
-            return 0;
-        }
-        else if (low >= l && high <= h){
-            return val;
-        }
-        else{
-            return left->get_range(l, h) + right->get_range(l, h);
-        }
-    }
-
-    void update_range(int l, int h, $type$ diff){
-        push_down();
-        if (low > h || high < l){
-            return;
-        }
-        if (low >= l && high <= h){
-            lazy += diff;
-            push_down();
-        }
-        else{
-            left->update_range(l, h, diff);
-            right->update_range(l, h, diff);
+    segmentTree(int l, int h, bool bl = true){
+        low = l;
+        high = h;
+        val = Data();
+        lazy = Lazy();
+        prop = false;
+        init = bl;
+        if (l < h){
+            int mid = (low + high)/2;
+            left = (segmentTree *)malloc(sizeof(segmentTree));
+            right = (segmentTree *)malloc(sizeof(segmentTree));
+            *left = segmentTree(low, mid, false);
+            *right = segmentTree(mid+1, high, false);
             update_val();
         }
     }
 
-    void change_val(int k, $type$ vl){
+    void get(){
         push_down();
+        assign(val, result);
+    }
+
+    void get_range(int l, int h){
+        if (init){
+            assign(idt, result);
+        }
+        push_down();
+
+        if (low > h || high < l){
+            return;
+        }
+        else if (low >= l && high <= h){
+            join(result, val);
+            assign(join_res, result);
+        }
+        else{
+            left->get_range(l, h);
+            right->get_range(l, h);
+        }
+    }
+
+    void update_range(int l, int h, Lazy &lz){
+        push_down();
+
+        if (low > h || high < l){
+            return;
+        }
+        else if (low >= l && high <= h){
+            join_lazy(lz, lazy);
+            lazy = join_res_l;
+            prop = true;
+            push_down();
+        }
+        else{
+            left->update_range(l, h, lz);
+            right->update_range(l, h, lz);
+            update_val();
+        }
+    }
+
+    void change_val(int k, Data &vl){
+        push_down();
+
         if (low > k || high < k){
             return;
         }
         else if (low == high){
-            val = vl;
+            assign(vl, val);
         }
         else{
             left->change_val(k, vl);
